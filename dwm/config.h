c84e5a20a369ea9f0d68dd95e19d3fa2996350d9
yusuf <yusufkhalifadev@gmail.com>
2024-02-14 10:17:05 +0200

    changes

--- dwm/config.h
+++ dwm/config.h
@@ -9,7 +9,7 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMonoNF:size=10:autohint=true:antialias=true" };
 //static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -28,9 +28,10 @@ static const char *const autostart[] = {
 	"doas", "cpupower", "frequency-set", "-gperformance", NULL,
 	"sxhkd", NULL,
 	"dunst", NULL,
-	"/usr/libexec/polkit-gnome-authentication-agent-1", NULL,
+	"/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1", NULL,
 	"gnome-keyring-daemon", "-d", NULL,
-	"feh", "--bg-scale", "/mnt/media/wall/Morgott-EldenRing.jpg", NULL,
+	"feh", "--bg-scale", "/mnt/media/wall/garden.jpg", NULL,
+	"gammastep", "-l", "30.0588:31.2268", "-t", "4400K:3200K", NULL,
 	"dwmblocks", NULL,
 	NULL /* terminate */
 };
