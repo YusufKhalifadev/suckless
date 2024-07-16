9ac9502c303299f15085dc8a718813c3c5591a9b
YusufKhalifa <yusufkhalifadev@gmail.com>
2024-07-16 03:49:11 +0300

    vol

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+  {"Vol:", "pamixer --get-volume",  10,   0},
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
